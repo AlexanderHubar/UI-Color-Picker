cdec76f1b703df5b0a6ee7abd23ccc7e7e6e924b
Olexandr Hubar <alexandr.hubar1999@gmail.com>
2019-06-18 14:38:41 +0300

    add snackbar and styled select

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import ColorBox from "../ColorBox/ColorBox";
 import Navbar from "../Navbar/Navbar";
+import Snackbar from "@material-ui/core/Snackbar";
 import "./Palette.css";
 
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500, format: "hex" };
+    this.state = { level: 500, format: "hex", formatChanged: false };
     this.handleSliderChange = this.handleSliderChange.bind(this);
     this.changeFormat = this.changeFormat.bind(this);
   }
@@ -16,23 +17,43 @@ class Palette extends Component {
     });
   }
   changeFormat(evt) {
-    this.setState({ format: evt.target.value });
+    this.setState({ format: evt.target.value, formatChanged: true }, () => {
+      setTimeout(() => {
+        this.setState({ formatChanged: false });
+      }, 1500);
+    });
   }
   render() {
     const { colors } = this.props.palette;
-    const { level, format } = this.state;
+    const { level, format, formatChanged } = this.state;
     const ColorBoxes = colors[this.state.level].map(color => (
-      <ColorBox background={color[this.state.format]} name={color.name} />
+      <ColorBox
+        key={color.name}
+        background={color[this.state.format]}
+        name={color.name}
+      />
     ));
     return (
       <div className="Palette">
         <Navbar
           level={level}
           changeLevel={this.handleSliderChange}
-					handleChange={this.changeFormat}
-					format={format}
+          handleChange={this.changeFormat}
+          format={format}
         />
         <div className="Palette-colors">{ColorBoxes}</div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={formatChanged}
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          message={
+            <span id="message-id">
+              Format Changed to {format.toUpperCase()}!
+            </span>
+          }
+        />
       </div>
     );
   }
